64ec9cc6d8a10782fd37ab85568853fd3be3d751
Ksenaaa <oksenaka@gmail.com>
2025-07-18 08:20:43 +0200

    deleted unnecessary files

--- client/src/middleware/authMiddleware.ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export function authMiddleware(request: NextRequest) {
-    const token = request.cookies.get('token');
-    if (!token) {
-        return NextResponse.redirect(new URL('/login', request.url));
-    }
-    return NextResponse.next();
-}
